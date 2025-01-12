a79ea458b667b6a0167985c886d9b92670bf717e
dmitrykostenkoweb <dmitrykostenkoweb@gmail.com>
2025-01-12 22:16:40 +0100

    added project models

--- src/models/project.ts
+++ src/models/project.ts
@@ -1,6 +1,19 @@
- // export interface Project {
- //    projectId: number;
- //    name: string;
- //    description?: string;
- //    imageUrl?: string;
- // }
+import { Status } from "@/models/shared";
+
+export interface RawProject {
+  id: number;
+  name: string;
+  status: Status;
+  description?: string;
+  image_url?: string;
+  area_id: number;
+}
+
+export interface Project {
+  id: number;
+  name: string;
+  status: Status;
+  description?: string;
+  imageUrl?: string;
+  areaId: number;
+}
